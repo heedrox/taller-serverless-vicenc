df080e4160c3bf92a7588efd70eb739ed8346fe9
jmarti <jmarti@theinit.com>
2019-01-24 13:09:56 +0100

    green local test

--- tests/getMasters.spec.js
+++ tests/getMasters.spec.js
@@ -1,6 +1,10 @@
 const when = require("./steps/when");
+const { init } = require("./steps/init");
 
 describe(`When we invoke the GET /masters endpoint`, () => {
+  beforeAll(() => {
+    init();
+  });
   test(`Should return an array of 8 masters`, async () => {
     const res = await when.we_invoke_get_masters();
 
---
+++ tests/steps/init.js
@@ -0,0 +1,4 @@
+module.exports.init = () => {
+  process.env.mastersTable = "masters-dev";
+  process.env.AWS_REGION = "eu-west-1";
+};

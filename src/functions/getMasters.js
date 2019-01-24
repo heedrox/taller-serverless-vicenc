67fd98600fa8873e03abfde20db94956e0d8cb3b
jmarti <jmarti@theinit.com>
2019-01-24 12:55:49 +0100

    add resources dynamodb

--- src/functions/getMasters.js
+++ src/functions/getMasters.js
@@ -1,9 +1,8 @@
 module.exports.handler = async event => {
-  const { name } = event.pathParameters;
 
   const res = {
     statusCode: 200,
-    body: JSON.stringify(`Hello ${name}`)
+    body: JSON.stringify(`Hello`)
   };
 
   return res;
